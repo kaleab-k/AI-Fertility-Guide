314ef1a03650684471726bf70dd5cc64e5ad60de
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-13 17:20:18 -0400

    first commit

--- response.py
+++ response.py
@@ -41,7 +41,7 @@ def display_response(user_data=None, openai_api_key=None):
         # client = OpenAI(api_key=openai_api_key)
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
-        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        response = client.chat.completions.create(model="gpt-4-turbo", messages=st.session_state.messages)
         msg =  response.choices[0].message.content
         st.session_state.messages.append({"role": "assistant", "content": msg})
         st.chat_message("assistant").write(msg)
