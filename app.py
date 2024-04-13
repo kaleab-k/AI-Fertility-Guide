c55c31c6a3f8193c0bcdc81e73667e1a64c2f6e6
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-13 16:09:58 -0400

    first commit

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ def main():
     with st.sidebar:
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
     if app_mode == "Welcome":
         st.session_state.current_page = 'welcome'
@@ -38,7 +36,7 @@ def main():
     elif st.session_state['current_page'] == 'questions':
         questionnaire()
     elif st.session_state['current_page'] == 'chat':
-        chat()
+        display_response()
     elif st.session_state['current_page'] == 'response':
         display_response()
 
