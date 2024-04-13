1288bd47dc35418dac14cfcc7cbb2b00762320cb
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-13 16:33:16 -0400

    first commit

--- response.py
+++ response.py
@@ -30,21 +30,21 @@ def display_response(user_data=None, openai_api_key=None):
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "How can I further assist you?"}]
 
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
+    # for msg in st.session_state.messages:
+    #     st.chat_message(msg["role"]).write(msg["content"])
 
-    if prompt := st.chat_input():
-        # if not openai_api_key:
-        #     st.info("Please add your OpenAI API key to continue.")
-        #     st.stop()
+    # if prompt := st.chat_input():
+    #     # if not openai_api_key:
+    #     #     st.info("Please add your OpenAI API key to continue.")
+    #     #     st.stop()
 
-        # client = OpenAI(api_key=openai_api_key)
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").write(prompt)
-        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-        msg =  response.choices[0].message.content
-        st.session_state.messages.append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
+    #     # client = OpenAI(api_key=openai_api_key)
+    #     st.session_state.messages.append({"role": "user", "content": prompt})
+    #     st.chat_message("user").write(prompt)
+    #     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    #     msg =  response.choices[0].message.content
+    #     st.session_state.messages.append({"role": "assistant", "content": msg})
+    #     st.chat_message("assistant").write(msg)
 
 def save_feedback(user_data, feedback):
     # Placeholder for saving feedback
