43918bcceabef0feb75f5efa93c1a2ecbc5f172d
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-13 13:37:28 -0400

    first commit

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def questionnaire():
         st.session_state.page_number = 0  # Reset for reusability
 
 def navigate():
-    col1, _, _,_, col2 = st.columns(5)
+    col1, _, _,_,_, col2 = st.columns(6)
     if col1.button("Back"):
         if st.session_state.page_number > 0:
             st.session_state.page_number -= 1
