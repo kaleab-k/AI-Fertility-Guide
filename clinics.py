18471f98f1a18da0d7c1e31e0e8cdfebd890e0fd
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-14 01:59:52 -0400

    first commit

--- clinics.py
+++ clinics.py
@@ -4,6 +4,8 @@ from assistant import PetalAssitant
 import pandas as pd
 import json
 
+import pydeck as pdk
+
 def clinics():
 
     zip_code = st.session_state.get('zip_code', 'Not specified')
@@ -19,8 +21,45 @@ def clinics():
     # Convert dictionary to pandas DataFrame
     clinics_df = pd.DataFrame(res['clinics'])
 
-    # Display the map
-    st.map(clinics_df)
+    # # Display the map
+    # st.map(clinics_df)
+    # Function to join services into a single string
+    clinics_df['services_offered'] = clinics_df['services_offered'].apply(lambda x: ', '.join(x))
+
+    # Set up PyDeck layer for the map
+    layer = pdk.Layer(
+        'TextLayer',  # Use TextLayer to show text
+        clinics_df,
+        pickable=True,
+        get_position='[lo, lat]',
+        get_text='name',  # Clinic name will be shown on the map
+        get_size=16,
+        get_color=[255, 0, 0],  # RGB color of the text
+        get_angle=0,
+        # Note that text alignment and anchor are necessary for TextLayer
+        get_text_anchor='"middle"',
+        get_alignment_baseline='"center"'
+    )
+
+    # Define the initial view for the PyDeck map
+    view_state = pdk.ViewState(
+        latitude=clinics_df['lat'].mean(),
+        longitude=clinics_df['lon'].mean(),
+        zoom=11,
+        pitch=0
+    )
+
+    # Render PyDeck map in Streamlit
+    st.pydeck_chart(pdk.Deck(
+        layers=[layer],
+        initial_view_state=view_state,
+        tooltip={
+            'html': '<b>Name:</b> {name}<br /><b>Services Offered:</b> {services_offered}',
+            'style': {
+                'color': 'white'
+            }
+        }
+    ))
 
 
     
