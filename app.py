52d8d32c36126f74df5167bd4294d3b14082eff7
Kaleab Kinfu <kinfu@jhu.edu>
2024-04-13 22:47:24 -0400

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from figma import figma_ui
 
 app_mode = "Welcome"
 
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
+
 def main():
     # Set page config for wide mode
     st.set_page_config(
